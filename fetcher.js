dc66ae17487c3521a6c1d8366a12f4bf41d12e3f
Penguuuuu <PenguGithub@gmail.com>
2025-08-07 03:18:02 +1000

    Change script names

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Titanic Completion
+// @version      1.4
+// @author       Patchouli
+// @match        https://osu.titanic.sh/u/*
+// @match        https://osu.titanic.sh/account/settings/*
+// @include      https://osu.titanic.sh/rankings/*/clears*
+// @grant        GM_xmlhttpRequest
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @updateURL    https://raw.githubusercontent.com/Penguuuuu/titanic-completion/main/main.js
+// @downloadURL  https://raw.githubusercontent.com/Penguuuuu/titanic-completion/main/main.js
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+  method: "GET",
+  url: "https://raw.githubusercontent.com/Penguuuuu/titanic-completion/main/script.js",
+  onload: function(response) {
+    eval(response.responseText);
+  }
+});
